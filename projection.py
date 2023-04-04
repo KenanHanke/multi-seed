77368c7c742f797fdb80d57a894884d5d364052b
ken <k.git.info@gmail.com>
2023-04-04 12:59:33 +0200

    Add detailed plan of action in comments

--- projection.py
+++ projection.py
@@ -1,6 +1,21 @@
 class Projection:
     """
-    Function that fulfills feature projection. Not guaranteed to be
-    idempotent in the formal mathematical sense of a projection, but is
-    so in the case of PCA.
+    Function that fulfills feature projection; not idempotent in the
+    formal mathematical sense of a projection.
     """
+
+    # I should perform PCA on the dataset without normalizing it
+    # at all, because correlation coefficients are already normalized
+    # to the range [-1, 1] and it makes sense that a correlation
+    # coefficient of 0 should stay 0 after PCA.
+
+    # After PCA, I should perform absolute value on the result, because
+    # a strong negative correlation is just as informative for connection
+    # detection as a strong positive correlation (inhibitory vs. excitatory
+    # connections). A positive side effect of this is that cerebrospinal
+    # fluid and similarly disconnected regions will be projected to the
+    # origin and be black in the result.
+
+    # I believe that performing absolute value after PCA is permissible
+    # because PCA is a linear transformation and no transformation
+    # took place beforehand. I NEED TO CHECK THIS!
