83153348f40be979a2595b7f244cc1416d7065e5
ken <k.git.info@gmail.com>
2023-04-11 00:44:07 +0200

    Refactor and add seed regions in addition to points

--- common.py
+++ common.py
@@ -3,22 +3,59 @@ from dataset import Dataset
 from concurrent.futures import ThreadPoolExecutor
 from typing import Iterable
 import gc
+from rbloom import Bloom
+import numpy as np
+from typing import Optional
+from image import Mask
 
 
-def create_combined_mask(mask_path, dataset_generator: Iterable[Dataset]):
+def sample_points(n_points: int, *, mask: Optional[Mask], rng=None):
+    """
+    Sample n_points from the given mask. If no mask is given, sample from the whole space.
+    Each point is guaranteed to be unique.
+
+    Args:
+        n_points (int): The number of points to sample.
+        mask (Mask, optional): A mask object to filter points based on certain criteria.
+        rng (np.random.Generator, optional): A random number generator to use for sampling points.
+
+    Returns:
+        np.array: An array containing the 3D coordinates of the sampled points. The shape of the array is (n_points, 3).
+    """
+    logging.info('Sampling %d unique points', n_points)
+
+    points = np.empty((n_points, 3), dtype=np.int16)
+
+    if rng is None:
+        rng = np.random.default_rng()
+
+    points_so_far = Bloom(n_points*1000, 0.01)
+
+    i = 0
+    while i < n_points:
+        point = tuple(rng.integers(mask.dimensions, size=3))
+        if mask is None or mask.data[point]:
+            if point not in points_so_far:
+                points[i] = point
+                points_so_far.add(point)
+                i += 1
+
+    return points
+
+
+def create_combined_mask(mask_path, datasets: Iterable[Dataset]):
     """
     Accumulate a mask from the given datasets.
     """
     logging.info('Creating combined mask from datasets')
-    dataset_generator = iter(dataset_generator)
+    datasets = iter(datasets)
     try:
-        first = next(dataset_generator)
+        first = next(datasets)
     except StopIteration:
         raise ValueError('No datasets given')
     mask = first.extract_mask()
-    for dataset in dataset_generator:
+    for dataset in datasets:
         mask |= dataset.extract_mask()
-        del dataset  # free memory
     mask.save(mask_path)
 
 
--- reference.py
+++ reference.py
@@ -1,24 +1,33 @@
 from rbloom import Bloom
 import numpy as np
+from numba import njit, prange
 import logging
 from image import Mask
+from common import sample_points
 
 
 class ReferenceBuilder:
     """
     A class to create a set of reference time series by sampling points from a dataset.
 
-    This class samples points from a dataset based on the provided dimensions and an optional mask. 
-    The sampled points can be used to build a Reference object, which contains a set of reference 
+    This class samples points from a dataset based on the provided dimensions and an optional mask.
+    The sampled points can be used to build a Reference object, which contains a set of reference
     time series that can be used to compute correlation coefficients with other time series.
+
+    If the radius is greater than 0, the sampled points will be interpreted as the center of a
+    spherical seed region and the time series for that region will be the weighted mean of all
+    voxels within that sphere. The weights are determined by w = - d^2 / r^2 + 1, where d is the
+    distance from the center of the sphere and r is the radius of the sphere. This means that
+    voxels closer to the center of the sphere will have a higher weight than voxels further away.
 
     Attributes:
         dimensions (tuple[int]): The dimensions of the data to sample points from.
         mask (Mask, optional): A mask object to filter points based on certain criteria.
         points (np.array): An array containing the sampled points.
+        radius (float): The radius of the seed regions to sample. Must be greater than or equal to 0.
     """
 
-    def __init__(self, *, dimensions: tuple[int] = None, mask: Mask = None):
+    def __init__(self, *, radius: float, dimensions: tuple[int] = None, mask: Mask = None):
         """
         Initialize a ReferenceBuilder instance.
 
@@ -28,14 +37,16 @@ class ReferenceBuilder:
 
         Note: Either dimensions or mask must be specified.
         """
+        self.radius = float(radius)
         self.mask = mask
         if mask is None:
             self.dimensions = dimensions
         else:
             self.dimensions = mask.dimensions
         self.points = None
+        self._loaded = False
 
-    def sample(self, n: int, rng=None):
+    def sample(self, n_points: int, rng=None):
         """
         Sample n points from the data space.
 
@@ -43,23 +54,11 @@ class ReferenceBuilder:
             n (int): The number of points to sample.
             rng (np.random.Generator, optional): A random number generator to use for sampling points.
         """
-        logging.info('Sampling %d points', n)
+        if self._loaded:
+            raise RuntimeError(
+                'Cannot sample new points from a loaded ReferenceBuilder instance')
 
-        self.points = np.empty((n, 3), dtype=np.int16)
-
-        if rng is None:
-            rng = np.random.default_rng()
-
-        points_so_far = Bloom(n*1000, 0.01)
-
-        i = 0
-        while i < n:
-            point = tuple(rng.integers(self.dimensions, size=3))
-            if self.mask is None or self.mask.data[point]:
-                if point not in points_so_far:
-                    self.points[i] = point
-                    points_so_far.add(point)
-                    i += 1
+        self.points = sample_points(n_points, mask=self.mask, rng=rng)
 
     def save(self, path):
         """
@@ -68,7 +67,8 @@ class ReferenceBuilder:
         Args:
             path (str): The path to save the compressed file.
         """
-        np.savez_compressed(path, points=self.points)
+        np.savez_compressed(path, points=self.points,
+                            radius=self.radius, dimensions=self.dimensions)
 
     @classmethod
     def load(cls, path):
@@ -82,8 +82,12 @@ class ReferenceBuilder:
             ReferenceBuilder: An instance of ReferenceBuilder with the loaded data.
         """
         data = np.load(path)
-        reference_builder = cls()
-        reference_builder.points = data['points']
+        points = data['points']
+        radius = data['radius']
+        dimensions = tuple(data['dimensions'])
+        reference_builder = cls(radius=radius, dimensions=dimensions)
+        reference_builder.points = points
+        reference_builder._loaded = True
         return reference_builder
 
     def __len__(self):
@@ -103,9 +107,56 @@ class ReferenceBuilder:
 
         reference = Reference(len(self), dataset.time_series_length)
         reference.source = self
-        for i, point in enumerate(self.points):
-            reference.data[i] = dataset.data[tuple(point)]
+        _build_reference(reference.data, dataset.data, self.points, self.radius)
         return reference
+
+
+############ START OF COMPILED REFERENCEBUILDER HELPER FUNCTIONS ############
+@njit(parallel=True)
+def _build_reference(reference_data, dataset_data, points, radius):
+    for i in prange(len(points)):
+        point = points[i]
+        point = (int(point[0]),
+                 int(point[1]),
+                 int(point[2]))  # necessary syntax for numba
+        if radius <= 0:
+            reference_data[i] = dataset_data[point]
+        else:
+            reference_data[i] = _build_reference_seed(dataset_data, point, radius)
+
+
+@njit
+def _build_reference_seed(dataset_data, point, radius):
+    """
+    Returns a time series where each value is the weighted mean of all voxels of the
+    corresponding time point within a sphere of radius r centered at the given point.
+    """
+    x, y, z = point  # not necessarily MNI x, y, z coordinates
+    ceil_radius = int(np.ceil(radius))
+
+    # Prevent going out of bounds of the dataset
+    min_x = max(0, x - ceil_radius)
+    max_x = min(dataset_data.shape[0] - 1, x + ceil_radius)
+    min_y = max(0, y - ceil_radius)
+    max_y = min(dataset_data.shape[1] - 1, y + ceil_radius)
+    min_z = max(0, z - ceil_radius)
+    max_z = min(dataset_data.shape[2] - 1, z + ceil_radius)
+
+    total_weight = 0
+    time_series = np.zeros(dataset_data.shape[3], dtype=np.float64)
+
+    for i in range(min_x, max_x + 1):
+        for j in range(min_y, max_y + 1):
+            for k in range(min_z, max_z + 1):
+                dist = np.sqrt((i - x)**2 + (j - y)**2 + (k - z)**2)
+                weight = 1 - dist**2 / radius**2
+                weight *= weight > 0  # only use voxels within the sphere
+
+                total_weight += weight
+                time_series += weight * dataset_data[i, j, k]
+
+    return time_series / total_weight
+############ END OF COMPILED REFERENCEBUILDER HELPER FUNCTIONS ############
 
 
 class Reference:
@@ -118,13 +169,13 @@ class Reference:
         source (ReferenceBuilder): The ReferenceBuilder instance used to generate the reference time series.
     """
 
-    def __init__(self, n: int, time_series_length: int):
+    def __init__(self, n_seeds: int, time_series_length: int):
         """
         Initialize an empty Reference instance.
 
         Args:
-            n (int): The number of reference time series.
+            n_seeds (int): The number of reference time series.
             time_series_length (int): The length of each time series.
         """
-        self.data = np.zeros((n, time_series_length), dtype=np.float64)
+        self.data = np.zeros((n_seeds, time_series_length), dtype=np.float64)
         self.source: ReferenceBuilder = None
