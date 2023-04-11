781baef94b9abc84100ba1375058808656466bc9
ken <k.git.info@gmail.com>
2023-04-11 20:15:29 +0200

    Formatting

--- dataset.py
+++ dataset.py
@@ -7,7 +7,14 @@ from tift import TIFT_DTYPE
 
 
 class Dataset:
-    def __init__(self, dimensions: tuple[int] = None, time_series_length: int = None, *, dtype=None, data=None):
+    def __init__(
+        self,
+        dimensions: tuple[int] = None,
+        time_series_length: int = None,
+        *,
+        dtype=None,
+        data=None
+    ):
         if data is not None:
             self.data = data
         else:
@@ -17,27 +24,30 @@ class Dataset:
 
     @classmethod
     def load(cls, file_path):
-        logging.info('Loading dataset from %s', file_path)
-        data = np.load(file_path)['data']
+        logging.info("Loading dataset from %s", file_path)
+        data = np.load(file_path)["data"]
         return cls(data=data)
 
     def save(self, file_path):
-        logging.info('Saving dataset to %s', file_path)
+        logging.info("Saving dataset to %s", file_path)
         np.savez_compressed(file_path, data=self.data)
 
     @classmethod
     def load_tift(cls, folder_path):
-        logging.info('Loading dataset from folder %s', folder_path)
+        logging.info("Loading dataset from folder %s", folder_path)
 
         # retrieve all image paths in folder
-        pattern = r'f\d{10}\.img\.z'
+        pattern = r"f\d{10}\.img\.z"
         listing = os.listdir(folder_path)
-        img_paths = [os.path.join(folder_path, entry)
-                     for entry in listing if re.fullmatch(pattern, entry)]
+        img_paths = [
+            os.path.join(folder_path, entry)
+            for entry in listing
+            if re.fullmatch(pattern, entry)
+        ]
         img_paths.sort()
 
         time_series_length = len(img_paths)
-        dimensions = (256,)*3
+        dimensions = (256,) * 3
 
         # initialize dataset
         dataset = cls(dimensions, time_series_length, dtype=TIFT_DTYPE)
@@ -48,8 +58,8 @@ class Dataset:
 
         return dataset
 
-    def save_tift(self, folder_path, filename_format='f{one_based_index:010}.img.z'):
-        logging.info('Saving dataset to folder %s', folder_path)
+    def save_tift(self, folder_path, filename_format="f{one_based_index:010}.img.z"):
+        logging.info("Saving dataset to folder %s", folder_path)
 
         # create folder if it does not exist
         if not os.path.exists(folder_path):
@@ -59,7 +69,8 @@ class Dataset:
         for i in range(self.time_series_length):
             img = Image(data=self.data[..., i])
             img_path = os.path.join(
-                folder_path, filename_format.format(one_based_index=i+1))
+                folder_path, filename_format.format(one_based_index=i + 1)
+            )
             img.save_tift(img_path)
 
     def extract_mask(self):
@@ -80,7 +91,8 @@ class Dataset:
         return self.data.dtype
 
     def copy(self):
-        new_dataset = self.__class__(self.dimensions, self.time_series_length,
-                                     dtype=self.dtype)
+        new_dataset = self.__class__(
+            self.dimensions, self.time_series_length, dtype=self.dtype
+        )
         new_dataset.data[:] = self.data
         return new_dataset
