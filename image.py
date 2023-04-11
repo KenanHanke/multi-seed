09ea1a081d235411ccd5b3d0d4b2814a0cd5206f
ken <k.git.info@gmail.com>
2023-04-11 20:16:03 +0200

    Formatting

--- image.py
+++ image.py
@@ -13,29 +13,29 @@ class Image:
 
     @classmethod
     def load(cls, path):
-        logging.debug('Loading image from %s', path)
-        data = np.load(path)['data']
+        logging.debug("Loading image from %s", path)
+        data = np.load(path)["data"]
         return cls(data=data)
 
     def save(self, path):
-        logging.debug('Saving image to %s', path)
+        logging.debug("Saving image to %s", path)
         np.savez_compressed(path, data=self.data)
 
     @classmethod
     def load_tift(cls, path):
-        logging.debug('Loading image from %s', path)
+        logging.debug("Loading image from %s", path)
 
-        dimensions = (256,)*3
+        dimensions = (256,) * 3
         image = cls(dimensions, dtype=TIFT_DTYPE)
-        with gzip.open(path, 'rb') as f:
+        with gzip.open(path, "rb") as f:
             raw_data = np.frombuffer(f.read(), dtype=TIFT_DTYPE)
             image.data[:] = raw_data.reshape(dimensions)
         return image
 
     def save_tift(self, path):
-        logging.debug('Saving image to %s', path)
+        logging.debug("Saving image to %s", path)
 
-        with gzip.open(path, 'wb') as f:
+        with gzip.open(path, "wb") as f:
             f.write(self.data.tobytes())
 
     @property
@@ -56,11 +56,11 @@ class Mask(Image):
     def __init__(self, dimensions: tuple[int] = None, *, dtype=bool, data=None):
         super().__init__(dimensions, dtype=dtype, data=data)
 
-    def __ior__(self, other: 'Mask'):
+    def __ior__(self, other: "Mask"):
         self.data |= other.data
         return self
 
-    def __or__(self, other: 'Mask'):
+    def __or__(self, other: "Mask"):
         new_mask = self.copy()
         new_mask |= other
         return new_mask
