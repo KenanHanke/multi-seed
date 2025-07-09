51a039ccd797533a14be32c057bed53e9ea460d5
KenanHanke <k.git.info@gmail.com>
2025-07-09 20:27:23 +0200

    Cleanup and refactoring

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ from config import Config
 from dataset import Dataset
 from image import Image, Mask
 from reference import ReferenceBuilder
-import tift
+import io_utils
 from feature_mapper import name_to_mapper_class
 import os
+
+
+
+# suppress numba's excessive debug messages
+numba_logger = logging.getLogger("numba")
+numba_logger.setLevel(logging.WARNING)
 
 
 def exec_config(config: Config, rng=None):
@@ -21,7 +27,7 @@ def exec_config(config: Config, rng=None):
 
     # prepare the dataset loaders for each cohort
     cohort_loaders = {
-        cohort: tift.load_datasets(paths, asynchronous=parallel_io)
+        cohort: io_utils.load_datasets_lazy(paths, asynchronous=parallel_io)
         for cohort, paths in config.cohorts.items()
     }
 
@@ -29,6 +35,11 @@ def exec_config(config: Config, rng=None):
     all_dataset_loader = functools.reduce(operator.add,
                                           cohort_loaders.values())
     mask = all_dataset_loader.extract_mask()
+    if results_folder:
+        # save the mask to the results folder
+        mask_path = os.path.join(results_folder, "mask.img")
+        reformatted_mask = mask.converted(np.int16).scaled(255)
+        io_utils.save_image(reformatted_mask, mask_path)
 
     # prepare reference builder
     reference_builder = ReferenceBuilder(radius=config.params["SEED_RADIUS"],
@@ -37,14 +48,10 @@ def exec_config(config: Config, rng=None):
 
     if results_folder:
         # create visualization of reference builder
-        visualization = reference_builder.visualized().normalized()
-        visualization = visualization.scaled(256).converted(np.uint16)
-        visualization_folder = os.path.join(results_folder,
-                                            "seed_visualization")
-        os.makedirs(visualization_folder, exist_ok=True)
-        visualization_path = os.path.join(visualization_folder,
-                                          "visualization.img.z")
-        tift.save_image(visualization, visualization_path)
+        visualization = reference_builder.visualized().normalized().scaled(1023).converted(np.int16)
+        visualization_path = os.path.join(results_folder,
+                                          "seed_visualization.img")
+        io_utils.save_image(visualization, visualization_path)
 
     # initialize and fit mapper
     N_FEATURES = config.params["N_FEATURES"]
@@ -92,76 +99,13 @@ def exec_config(config: Config, rng=None):
             os.makedirs(norm_res_dir, exist_ok=True)
             image.save(os.path.join(norm_res_dir, f"{i}.npz"))
 
-    # also save the normalized results as ints scaled to [0, 4095]
-    # using the tift compatible format; 4095 is the maximum value
-    # that can be stored in a 12 bit image, which is the standard
-    for res_dir in res_dirs:
-        tift_res_dir = os.path.join(res_dir, "tift_normalized_scaled")
-        os.makedirs(tift_res_dir, exist_ok=True)
-
-        first = True
-        for i in range(N_FEATURES):
-            # load the normalized float result
-            image = Image.load(
-                os.path.join(
-                    res_dir,
-                    "normalized_float_result",
-                    f"{i}.npz",
-                ))
-
-            # scale to [0, 4095]
-            image = image.scaled(4095).converted(np.uint16)
-
-            if first:
-                # initialize the dataset
-                first = False
-                dimensions = image.dimensions
-                dtype = image.dtype
-                dataset = Dataset(dimensions, N_FEATURES, dtype=dtype)
-
-            # put the image into the dataset
-            dataset[i] = image
-
-        tift.save_dataset(dataset, tift_res_dir)
-
-    ####################################################################
-    ####################################################################
-    #        THIS IS WHERE GROUP COMPARISON WILL HAPPEN!!!!!!!!        #
-    ####################################################################
-    ####################################################################
-
 
 def main():
     logging.basicConfig(format="[%(levelname)s %(asctime)s] %(message)s",
                         level=logging.DEBUG)
 
-    # suppress numba's excessive debug messages
-    numba_logger = logging.getLogger("numba")
-    numba_logger.setLevel(logging.WARNING)
-
     # ensure reproducibility of random numbers
     rng = np.random.default_rng(seed=42)
-
-    # # dataset = tift.load_dataset(PATH)
-    # # dataset.save("dataset.npz")
-    # dataset = Dataset.load("dataset.npz")
-    # # mask = dataset.extract_mask()
-    # # mask.save("mask.npz")
-    # mask = Mask.load("mask_combined.npz")
-
-    # reference_builder = ReferenceBuilder(radius=20, mask=mask)
-    # reference_builder.sample(N, rng)
-    # visualization = reference_builder.visualized().normalized()
-    # visualization = visualization.scaled(256).converted(np.uint16)
-    # tift.save_image(visualization, "visualization.img.z")
-    # reference = reference_builder.build(dataset)
-
-    # pca = PCAMapper(NUM_OF_PRINCIPAL_COMPONENTS, reference)
-    # pca.fit([dataset],
-    #         samples_per_dataset=SIZE_OF_COMPARISON_POINT_SAMPLE,
-    #         rng=rng)
-    # result = pca.transform(dataset)
-    # tift.save_dataset(result, os.path.join(PATH, "pca"))
 
     with open("pdstudy.config", "r") as f:
         config_str = f.read()
