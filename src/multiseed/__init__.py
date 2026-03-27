741b24244548f0862217c5b8edfca21d8bf02e7c
KenanHanke <k.git.info@gmail.com>
2026-03-27 19:22:44 +0100

    Make convenience functions and classes available in base package namespace

--- src/multiseed/__init__.py
+++ src/multiseed/__init__.py
@@ -1,7 +1,9 @@
 # multiseed/__init__.py
 
 import logging
-
+from .image import Image, Mask
+from .dataset import Dataset
+from .ioutils import load_image, save_image, load_dataset, save_dataset
 
 # suppress numba's excessive debug messages
 numba_logger = logging.getLogger("numba")
