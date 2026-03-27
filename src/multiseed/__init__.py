ff481fc1d311da6d7fb527f8ac478b3f0cdce533
KenanHanke <k.git.info@gmail.com>
2026-03-27 21:21:38 +0100

    Minor refactor

--- src/multiseed/__init__.py
+++ src/multiseed/__init__.py
@@ -6,5 +6,4 @@ from .dataset import Dataset
 from .ioutils import load_image, save_image, load_dataset, save_dataset
 
 # suppress numba's excessive debug messages
-numba_logger = logging.getLogger("numba")
-numba_logger.setLevel(logging.WARNING)
+logging.getLogger("numba").setLevel(logging.WARNING)
