4960ba9223fdc2e9ee0d1946776c15cbaa44334f
KenanHanke <k.git.info@gmail.com>
2026-03-27 23:18:32 +0100

    Correct documentation

--- src/multiseed/__init__.py
+++ src/multiseed/__init__.py
@@ -6,11 +6,11 @@
 The `multiseed` library exposes an extensive API encompassing almost all parts of its algorithm.
 
 A few select functions and classes are also imported into the top-level namespace for convenience:
-- `Image`: the main image class, representing a 3D MRI image.
-- `Mask`: a subclass of `Image` representing a binary mask.
-- `Dataset`: a class representing a dataset, which is a collection of images and masks.
-- `load_image` and `save_image`: functions for loading and saving images.
-- `load_dataset` and `save_dataset`: functions for loading and saving datasets.
+- `Image`: the main image class, representing a 3D MRI image (`multiseed.image.Image`).
+- `Mask`: a subclass of `Image` representing a binary mask (`multiseed.image.Mask`).
+- `Dataset`: a class representing a dataset, which is a collection of images and masks from a single scan (`multiseed.dataset.Dataset`).
+- `load_image` and `save_image`: functions for loading and saving images (`multiseed.ioutils.load_image` and `multiseed.ioutils.save_image`).
+- `load_dataset` and `save_dataset`: functions for loading and saving datasets (`multiseed.ioutils.load_dataset` and `multiseed.ioutils.save_dataset`).
 
 See the submodule documentation pages for details.
 """
